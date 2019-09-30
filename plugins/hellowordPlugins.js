bb4c4f6f182754cd5c81af2a727d7c8912801574
linyining <linyining@myfriday.cn>
2019-09-30 10:57:08 +0800

    更新

---
+++ plugins/hellowordPlugins.js
@@ -0,0 +1,9 @@
+// 1 构造函数
+// 2 prototype中需要一个apply方法
+class HelloWorld{
+    apply(){
+        console.log('helloworld');
+    }
+}
+
+module.exports = HelloWorld
--- resource/README.md
+++ resource/README.md
@@ -22,7 +22,7 @@
 
 - webpack基础配置
 - webpack高级配置
-- webpack性能优化
+- webpack性能优化 
 - tapable钩子
 - AST抽象语法树的应用
 - webpack原理分析, 手写webpack
